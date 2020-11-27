d643be917377a1522f454d2f5dfdd5d6a6adbcf1
dahailu <ludahai19@163.com>
2020-11-27 17:45:05 +0800

    fix visulize.py bugs and add logo!

---
+++ dataset/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+================================================================
+Copyright (C) 2019 * Ltd. All rights reserved.
+PROJECT      :  CloudViewer-ML
+FILE_NAME    :  __init__.py
+AUTHOR       :  DAHAI LU
+TIME         :  2020/11/27 下午2:19
+PRODUCT_NAME :  PyCharm
+================================================================
+"""
