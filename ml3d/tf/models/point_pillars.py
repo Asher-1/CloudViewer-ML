d2def40c26ee487052a5419304ea13d7f2244712
ludahai <ludahai19@163.com>
2025-10-16 17:20:24 +0800

    fix code style

--- ml3d/tf/models/point_pillars.py
+++ ml3d/tf/models/point_pillars.py
@@ -242,8 +242,8 @@ class PointPillars(BaseModel):
         max_val = np.array(self.point_cloud_range[3:])
 
         points = points[np.where(
-            np.all(np.logical_and(points[:, :3] >= min_val,
-                                  points[:, :3] < max_val),
+            np.all(np.logical_and(points[:, :3] >= min_val, points[:, :3]
+                                  < max_val),
                    axis=-1))]
 
         new_data = {'point': points, 'calib': data['calib']}
@@ -258,8 +258,8 @@ class PointPillars(BaseModel):
             max_val = np.array(self.point_cloud_range[3:])
 
             points = points[np.where(
-                np.all(np.logical_and(points[:, :3] >= min_val,
-                                      points[:, :3] < max_val),
+                np.all(np.logical_and(points[:, :3] >= min_val, points[:, :3]
+                                      < max_val),
                        axis=-1))]
 
             new_data['full_point'] = points
